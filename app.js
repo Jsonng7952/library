56d7499f8464cf5fa758b7a70663ef4ebdc664f6
Jason Ng <jason.ng7952@gmail.com>
2022-02-17 17:27:05 -0500

    Added form to take in user input

--- app.js
+++ app.js
@@ -7,21 +7,63 @@ function Book(title, author, pages, isRead) {
     this.isRead = isRead;
 }
 
-function addBook(book) {
-    myLibrary.push(book);
+// Still need to implement data attribute to correspond 
+// to locate each book object.
+function displayBook(book) {
+    const newCard = document.createElement('div');
+    const newTitle = document.createElement('div');
+    const newAuthor = document.createElement('div');
+    const newPages = document.createElement('div');
+    newCard.classList.add('card');
+    newTitle.classList.add('title');
+    newAuthor.classList.add('author');
+    newPages.classList.add('pages');
+    //newCard.setAttribute('');
+    newTitle.textContent = `${book.title}`;
+    newAuthor.textContent = `${book.author}`;
+    newPages.textContent = `${book.pages}`;
+    newCard.appendChild(newTitle);
+    newCard.appendChild(newAuthor);
+    newCard.appendChild(newPages);
+    bookContainer.appendChild(newCard);    
+
+    myLibrary.push(book);   // Adds the book object into the array
+} 
+
+function createBook() {
+    // NEW BOOK button brings up a form here. 
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    const pages = document.getElementById('pages').value;
+    //const isRead = document.getElementById('title').value;
+
+    let book = new Book(title, author, pages, true);
+
+    displayBook(book);
 }
 
-console.log(Book);
-let book1 = new Book("book1", "author1", "1", true);
-let book2 = new Book("book2", "author2", "2", false);
-let book3 = new Book("book3", "author3", "3", true);
+function showForm(){
+    bookForm.style.display = "flex";
+}
 
-addBook(book1);
-addBook(book2);
-addBook(book3);
-
-function displayBook() {
-    for(let i = 0; i < myLibrary.length; i++){
-        
+function hideForm(event){
+    if ((event.target !== bookForm && event.target !== addBookBtn) || event.target == cancelBtn) {
+        bookForm.style.display = "none";
     }
-}   
+}
+
+const bookContainer = document.querySelector('.books');
+
+const addBookBtn = document.querySelector('.add-book-btn');
+addBookBtn.addEventListener('click', showForm);
+
+const submitBtn = document.querySelector('.submit-btn');
+submitBtn.addEventListener('click', createBook);
+
+const cancelBtn = document.querySelector('.cancel-btn');
+cancelBtn.addEventListener('click', hideForm);
+
+const bookForm = document.querySelector('#book-form');
+
+//window.addEventListener('click', hideForm);
+
