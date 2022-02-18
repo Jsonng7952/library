10fbb9f80ec97bcd04d8dd0fa05ebcb873073778
Jason Ng <jason.ng7952@gmail.com>
2022-02-18 18:00:41 -0500

    Added button for completion of book, added deletion button, added data attribute for each book

--- app.js
+++ app.js
@@ -7,39 +7,75 @@ function Book(title, author, pages, isRead) {
     this.isRead = isRead;
 }
 
-// Still need to implement data attribute to correspond 
-// to locate each book object.
 function displayBook(book) {
     const newCard = document.createElement('div');
-    const newTitle = document.createElement('div');
-    const newAuthor = document.createElement('div');
-    const newPages = document.createElement('div');
     newCard.classList.add('card');
+
+    // Delete button for each book.
+    const deleteBtnContainer = document.createElement('div');
+    deleteBtnContainer.classList.add('delete-btn-container');
+    const deleteBtn = document.createElement('button');
+    deleteBtn.classList.add('delete-btn');
+    deleteBtn.textContent = 'x';
+    deleteBtnContainer.appendChild(deleteBtn);
+    newCard.appendChild(deleteBtnContainer);
+
+    // Title and Author.
+    const bookMainContent = document.createElement('div');
+    bookMainContent.classList.add('book-main');
+    const newTitle = document.createElement('div');
     newTitle.classList.add('title');
+    const newAuthor = document.createElement('div');
     newAuthor.classList.add('author');
+    bookMainContent.appendChild(newTitle);
+    bookMainContent.appendChild(newAuthor);
+    newCard.appendChild(bookMainContent);
+
+    // Pages and Read Progress.
+    const bookFooter = document.createElement('div');
+    bookFooter.classList.add('book-footer');
+    const newPages = document.createElement('div');
     newPages.classList.add('pages');
-    //newCard.setAttribute('');
+    const newRead = document.createElement('div');
+    newRead.classList.add('isRead');
+    bookFooter.appendChild(newPages);
+    bookFooter.appendChild(newRead);
+    newCard.appendChild(bookFooter);
+
+    // Assign the new entry with with the data attribute.
+    newCard.dataset.key = `${myLibrary.length}`;
+
     newTitle.textContent = `${book.title}`;
     newAuthor.textContent = `${book.author}`;
-    newPages.textContent = `${book.pages}`;
-    newCard.appendChild(newTitle);
-    newCard.appendChild(newAuthor);
-    newCard.appendChild(newPages);
+    newPages.textContent = `Pages: ${book.pages}`;
+    newRead.textContent = `Finished: ${book.isRead}`;
+
     bookContainer.appendChild(newCard);    
 
     myLibrary.push(book);   // Adds the book object into the array
 } 
 
 function createBook() {
-    // NEW BOOK button brings up a form here. 
+    // Get the values from the form input
     const title = document.getElementById('title').value;
     const author = document.getElementById('author').value;
     const pages = document.getElementById('pages').value;
-    //const isRead = document.getElementById('title').value;
+    const isReadYes = document.getElementById('isReadYes').checked;
 
-    let book = new Book(title, author, pages, true);
-
+    let book;
+    if(isReadYes === true){
+        book = new Book(title, author, pages, "Yes");
+    }
+    else {
+        book = new Book(title, author, pages, "No");
+    }
+    
     displayBook(book);
+
+    // Clear form input fields
+    document.getElementById('title').value = '';
+    document.getElementById('author').value = '';
+    document.getElementById('pages').value = '';
 }
 
 function showForm(){
@@ -49,6 +85,11 @@ function showForm(){
 function hideForm(event){
     if ((event.target !== bookForm && event.target !== addBookBtn) || event.target == cancelBtn) {
         bookForm.style.display = "none";
+
+        // Clear form input fields
+        document.getElementById('title').value = '';
+        document.getElementById('author').value = '';
+        document.getElementById('pages').value = '';
     }
 }
 
